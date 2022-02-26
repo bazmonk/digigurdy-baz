676fc4c5b9c89ea5d4e0f95405e585065c505ffb
Basil Lalli <bdlalli@lbl.gov>
2022-02-25 16:04:00 -0800

    Added Note enum and NoteNum string array.
    This will allow easier MIDI num -> name and name -> MIDI num throughout 
    this.

--- digigurdy-baz.ino
+++ digigurdy-baz.ino
@@ -78,6 +78,10 @@
 #include <EEPROM.h>
 #include <MIDI.h> // The MIDI Library
 
+// Baz added these:
+#include <string>
+using namespace std
+
 // MIDI_CREATE_DEFAULT_INSTANCE();
 // Two lines below added from https://www.pjrc.com/teensy/td_libs_MIDI.html
 // DJ Add
@@ -10352,6 +10356,53 @@ void clearallMIDI() { // Precautionary step that turns everything off that might
   display.display();
 
 } // end of clearallMIDI
+
+// enum Note maps absolute note names to MIDI note numbers (middle C4 = 60), which range from
+// 0 to 127.
+enum Note {
+  c_1, c_1s, d_1, d_1s, e_1, f_1, f_1s, g_1, g_1s, a_1, a_1s, b_1,
+  c0, c0s, d0, d0s, e0, f0, f0s, g0, g0s, a0, a0s, b0,
+  c1, c1s, d1, d1s, e1, f1, f1s, g1, g1s, a1, a1s, b1,
+  c2, c2s, d2, d2s, e2, f2, f2s, g2, g2s, a2, a2s, b2,
+  c3, c3s, d3, d3s, e3, f3, f3s, g3, g3s, a3, a3s, b3,
+  c4, c4s, d4, d4s, e4, f4, f4s, g4, g4s, a4, a4s, b4,
+  c5, c5s, d5, d5s, e5, f5, f5s, g5, g5s, a5, a5s, b5,
+  c6, c6s, d6, d6s, e6, f6, f6s, g6, g6s, a6, a6s, b6,
+  c7, c7s, d7, d7s, e7, f7, f7s, g7, g7s, a7, a7s, b7,
+  c8, c8s, d8, d8s, e8, f8, f8s, g8, g8s, a8, a8s, b8,
+  c9, c9s, d9, d9s, e9, f9, f9s, g9
+};
+
+// string array NoteNum is the reverse of the above Note enum.  It maps MIDI note numbers to
+// screen-friendly note names.
+string NoteNum[] = {
+  "C-1", "C-1#", "D-1", "D-1#", "E-1", "F-1", "F-1#", "G-1", "G-1#", "A-1", "A-1#", "B-1",
+  "C0", "C0#", "D0", "D0#", "E0", "F0", "F0#", "G0", "G0#", "A0", "A0#", "B0",
+  "C1", "C1#", "D1", "D1#", "E1", "F1", "F1#", "G1", "G1#", "A1", "A1#", "B1",
+  "C2", "C2#", "D2", "D2#", "E2", "F2", "F2#", "G2", "G2#", "A2", "A2#", "B2",
+  "C3", "C3#", "D3", "D3#", "E3", "F3", "F3#", "G3", "G3#", "A3", "A3#", "B3",
+  "C4", "C4#", "D4", "D4#", "E4", "F4", "F4#", "G4", "G4#", "A4", "A4#", "B4",
+  "C5", "C5#", "D5", "D5#", "E5", "F5", "F5#", "G5", "G5#", "A5", "A5#", "B5",
+  "C6", "C6#", "D6", "D6#", "E6", "F6", "F6#", "G6", "G6#", "A6", "A6#", "B6",
+  "C7", "C7#", "D7", "D7#", "E7", "F7", "F7#", "G7", "G7#", "A7", "A7#", "B7",
+  "C8", "C8#", "D8", "D8#", "E8", "F8", "F8#", "G8", "G8#", "A8", "A8#", "B8",
+  "C9", "C9#", "D9", "D9#", "E9", "F9", "F9#", "G9"
+};
+
+// Function tunings() accepts no arguments and presents the user with a series of menu prompts
+// on-screen in order to set a tuning for the gurdy.  Function returns no data, however it modifies
+// these global variables.  These are int values that correspond to MIDI note numbers.  A table of
+// them can be found here: https://studiocode.dev/resources/midi-middle-c/
+//
+// NOTE: note numbers are not standard.  In this case we are using a middle C4 (note 60).
+// VARIABLES MODIFIED:
+//  chan1midi - high chanter
+//  chan2midi - low chanter
+//  trompmidi - trompette a.k.a. high drone
+//  dronemidi - drone (low)
+void tunings() {
+
+};
 
 void tunings() {
 
