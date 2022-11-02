1d7b89a81d47fb5c7e78b0d0e4f32f45650314d4
Basil Lalli <bazmonkey@gmail.com>
2022-11-01 21:36:46 -0700

    Moved tuning submenu to its own file

---
+++ tuning_screens.h
@@ -0,0 +1,20 @@
+#ifndef TUNING_SCREENS_H
+#define TUNING_SCREENS_H
+
+#include "common.h"
+#include "notes.h"
+
+bool tuning();
+
+void tuning_hi_melody();
+void tuning_low_melody();
+void tuning_drone();
+void tuning_tromp();
+
+void manual_tuning_screen();
+void tune_string_screen(GurdyString *this_string);
+
+void volume_screen();
+void change_volume_screen(GurdyString *this_string);
+
+#endif
