51ebb8119b6745005c6dc9552fae300437cd1e18
Basil Lalli <bazmonkey@gmail.com>
2022-11-02 20:17:22 -0700

    Added confirmation screens to the EX choices.

--- ex_screens.cpp
+++ ex_screens.cpp
@@ -26,11 +26,48 @@ void ex_btn_choice_screen() {
     if (my1Button->wasPressed()) {
       ex1Button->fn_choice_screen(1);
 
+      String disp_str = " -----EX Buttons---- \n"
+                        "                     \n"
+                        "                     \n"
+                        "      EX1 Button     \n"
+                        "        Saved!       \n"
+                        "                     \n"
+                        "                     \n"
+                        "                     \n";
+
+      print_screen(disp_str);
+      delay(1000);
+
     } else if (my2Button->wasPressed()) {
       ex2Button->fn_choice_screen(2);
 
+      String disp_str = " -----EX Buttons---- \n"
+                        "                     \n"
+                        "                     \n"
+                        "      EX1 Button     \n"
+                        "        Saved!       \n"
+                        "                     \n"
+                        "                     \n"
+                        "                     \n";
+
+      print_screen(disp_str);
+      delay(1000);
+
     } else if (my3Button->wasPressed()) {
       ex3Button->fn_choice_screen(3);
+
+
+      String disp_str = " -----EX Buttons---- \n"
+                        "                     \n"
+                        "                     \n"
+                        "      EX1 Button     \n"
+                        "        Saved!       \n"
+                        "                     \n"
+                        "                     \n"
+                        "                     \n";
+
+      print_screen(disp_str);
+      delay(1000);
 
     } else if (my4Button->wasPressed() || myXButton->wasPressed()) {
       return;
