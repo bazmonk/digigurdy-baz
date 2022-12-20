e3f3d321cdafb183bb6a5ffa86d4d50ebdc4c04e
Basil Lalli <bazmonkey@gmail.com>
2022-12-19 16:04:17 -0800

    last-min fix

--- gurdystring.cpp
+++ gurdystring.cpp
@@ -51,7 +51,11 @@ void GurdyString::soundOff() {
 #if !defined(USE_TRIGGER) && !defined(USE_TSUNAMI)
   MIDI.sendNoteOff(note_being_played, midi_volume, midi_channel);
 #else
-  trigger_obj.trackFade(note_being_played + (128 * (midi_channel - 1)), -20, 200, true);
+  if (trigger_volume > -60) {
+    trigger_obj.trackFade(note_being_played + (128 * (midi_channel - 1)), trigger_volume - 10, 200, true);
+  } else {
+    trigger_obj.trackFade(note_being_played + (128 * (midi_channel - 1)), -70, 200, true);
+  }
   //trigger_obj.trackStop(note_being_played + (128 * (midi_channel - 1)));
 #endif
   is_playing = false;
