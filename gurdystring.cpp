aa49604e7f63455da4d12cd6fb643caf0e272fb9
Basil Lalli <bazmonkey@gmail.com>
2023-01-02 19:09:46 -0800

    Fixes compile-time warning
    (works fine, but this makes the compiler happy).

--- gurdystring.cpp
+++ gurdystring.cpp
@@ -307,7 +307,7 @@ String GurdyString::getGrosString() {
   if (gros_mode == 2) {
     return getLongNoteNum(getOpenNote() - 7);
   };
-  if (gros_mode == 3) {
-    return getLongNoteNum(getOpenNote() - 12);
-  };
+  
+  return getLongNoteNum(getOpenNote() - 12);
+  
 };
